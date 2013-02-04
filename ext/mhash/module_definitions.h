2a9d78a87191520d6da00310aba0904427469e90
Thibaut Deloffre <t.deloffre@alphalink.fr>
2013-02-04 14:36:01 +0100

    Mistake in header.

--- ext/mhash/module_definitions.h
+++ ext/mhash/module_definitions.h
@@ -1,7 +1,7 @@
 #ifndef __MODULE_DEFINITIONS__
 #define __MODULE_DEFINITIONS__
 
-  void mhash_define_conts(VALUE *m_mhash);
+  void mhash_define_consts(VALUE *m_mhash);
   void mhash_define_functions(VALUE *m_mhash);
 
 #endif
