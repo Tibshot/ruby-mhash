bab2a2761c8030753b29872a959ee7d63cde4fee
Thibaut Deloffre <t.deloffre@alphalink.fr>
2013-02-04 10:24:43 +0100

    Update extconf.rb for verbose errors

--- ext/mhash/extconf.rb
+++ ext/mhash/extconf.rb
@@ -1,4 +1,8 @@
 require 'mkmf'
 
-have_library('mhash') or raise
+have_header('stdio.h') or raise "stdio.h did't find. C library header needed. Check your compiler setup."
+have_header('ruby.h') or raise "ruby.h did't find. Did you install ruby environment development ?"
+have_header('mhash.h') or raise "mhash.h did't find. Check your libmhash setup."
+have_library('mhash') or raise "libmhash did't find. Install it."
+
 create_makefile('mhash/mhash')
