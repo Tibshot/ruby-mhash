b0569e508afdc27ccf3c16f1f41921707e857961
Thibaut Deloffre <t.deloffre@alphalink.fr>
2013-02-05 14:14:05 +0100

    Delete stdio.h dependance

--- ext/mhash/extconf.rb
+++ ext/mhash/extconf.rb
@@ -1,6 +1,5 @@
 require 'mkmf'
 
-have_header('stdio.h') or raise "stdio.h did't find. C library header needed. Check your compiler setup."
 have_header('ruby.h') or raise "ruby.h did't find. Did you install ruby environment development ?"
 have_header('mhash.h') or raise "mhash.h did't find. Check your libmhash setup."
 have_library('mhash') or raise "libmhash did't find. Install it."
